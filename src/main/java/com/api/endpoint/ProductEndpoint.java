d57a54fae309e926195d039a171794bcffbbac00
ElenaPavlova <hell618lena@gmail.com>
2019-09-06 11:59:05 +0500

    Refactoring

--- src/main/java/com/api/endpoint/ProductEndpoint.java
+++ src/main/java/com/api/endpoint/ProductEndpoint.java
@@ -1,5 +1,6 @@
 package com.api.endpoint;
 
+import com.api.product.schema.Product;
 import com.api.service.ServiceProduct;
 import com.api.product.schema.GetProductResponse;
 import com.api.product.schema.GetProductRequest;
@@ -8,6 +9,9 @@ import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Endpoint
 public class ProductEndpoint {
@@ -23,14 +27,15 @@ public class ProductEndpoint {
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getProductRequest")
     @ResponsePayload
     public GetProductResponse getProduct(@RequestPayload GetProductRequest request) {
-        GetProductResponse response = new GetProductResponse();
-        response.setCount(serviceProduct.getCount());
-
+        List<Product> productList = new ArrayList<>();
         if(!request.getName().isEmpty()) {
-            response.getProduct().addAll(serviceProduct.findByNameContainingIgnoreCase(request.getName()));
-            return response;
+            productList = serviceProduct.findByNameContainingIgnoreCase(request.getName());
+        } else {
+            productList = serviceProduct.findAll();
         }
-        response.getProduct().addAll(serviceProduct.findAll());
+        GetProductResponse response = new GetProductResponse();
+        response.getProduct().addAll(productList);
+        response.setCount(productList.size());
         return response;
 
     }
